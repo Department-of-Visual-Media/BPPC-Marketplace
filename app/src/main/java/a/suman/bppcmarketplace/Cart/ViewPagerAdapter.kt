7eb44a99368b78445fd641f391f88f7622c53a30
shivi2006 <f20190778@pilani.bits-pilani.ac.in>
2020-05-09 14:54:53 +0530

    Added ViewPager for Cart

---
+++ app/src/main/java/a/suman/bppcmarketplace/Cart/ViewPagerAdapter.kt
@@ -0,0 +1,27 @@
+package a.suman.bppcmarketplace.Cart
+
+import a.suman.bppcmarketplace.Cart.RecentBids.View.RecentBids
+import a.suman.bppcmarketplace.Cart.WishList.View.WishList
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class ViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+    override fun getItem(position: Int): Fragment {
+        return when (position) {
+            0 -> WishList()
+            else -> RecentBids()
+        }
+    }
+    override fun getCount(): Int {
+        return 2
+    }
+
+    override fun getPageTitle(position: Int): CharSequence {
+        return when (position) {
+            0 -> "WishList"
+            else -> "Cart"
+        }
+    }
+}
