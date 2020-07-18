2ce90756dc3bb384583f5570c5f266565cba8e3f
Ayush Suman <ayush231suman@gmail.com>
2020-07-18 12:21:06 +0530

    Double Check Singleton Database Lazy Initialisation

--- app/src/main/java/a/suman/bppcmarketplace/ApolloConnector.kt
+++ app/src/main/java/a/suman/bppcmarketplace/ApolloConnector.kt
@@ -4,41 +4,38 @@ import com.apollographql.apollo.ApolloClient
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 
-class ApolloConnector {
-    companion object {
-        private const val BASE_URL = "https://market.hedonhermdev.tech/api/graphql/"
+object ApolloConnector {
+    private const val BASE_URL = "https://market.hedonhermdev.tech/api/graphql/"
 
-        @Volatile
-        private var INSTANCE: ApolloClient? = null
+    @Volatile
+    private var INSTANCE: ApolloClient? = null
 
-        fun setUpApollo(): ApolloClient {
-            val instance = INSTANCE
-            if (instance == null) {
-                synchronized(this) {
-                    if (instance == null) {
-                        val interceptor = HttpLoggingInterceptor()
-                        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
-                        val okHttpClient = OkHttpClient.Builder().addInterceptor {
-                            val original = it.request()
-                            val builder =
-                                original.newBuilder().method(original.method, original.body)
-                            builder.header("Authorization", "JWT ${TokenClass.token}")
-                            it.proceed(builder.build())
-                        }.build()
-                        val temp =
-                            ApolloClient.builder().serverUrl(BASE_URL).okHttpClient(okHttpClient)
-                                .build()
-                        INSTANCE = temp
-                        return temp
-                    }
+    fun setUpApollo(): ApolloClient {
+        var instance = INSTANCE
+        if (instance == null) {
+            synchronized(this) {
+                if (instance == null) {
+                    val interceptor = HttpLoggingInterceptor()
+                    interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+                    val okHttpClient = OkHttpClient.Builder().addInterceptor {
+                        val original = it.request()
+                        val builder =
+                            original.newBuilder().method(original.method, original.body)
+                        builder.header("Authorization", "JWT ${TokenClass.token}")
+                        it.proceed(builder.build())
+                    }.build()
+                    instance =
+                        ApolloClient.builder().serverUrl(BASE_URL).okHttpClient(okHttpClient)
+                            .build()
+                    INSTANCE = instance
                 }
             }
-            return instance!!
         }
+        return instance!!
+    }
 
-        @Synchronized
-        fun invalidateApollo(){
-            INSTANCE = null
-        }
+    @Synchronized
+    fun invalidateApollo() {
+        INSTANCE = null
     }
 }
--- app/src/main/java/a/suman/bppcmarketplace/TokenClass.kt
+++ app/src/main/java/a/suman/bppcmarketplace/TokenClass.kt
@@ -1,17 +1,14 @@
 package a.suman.bppcmarketplace
 
-import android.app.Application
-import io.reactivex.Completable
-import io.reactivex.schedulers.Schedulers
-
 object TokenClass {
     var token: String? = null
-    fun updateToken(application: Application): Completable {
+
+    /*fun updateToken(application: Application): Completable {
         val authenticationServices =
             BPPCDatabase.getBPPCDatabase(application).getAuthenticationServices()
-        return authenticationServices.getBasicUserData().subscribeOn(Schedulers.io())
+            return authenticationServices.getBasicUserData().subscribeOn(Schedulers.io())
             .flatMapCompletable { Completable.complete().doOnComplete { token = it[0]!!.token } }
-    }
+    }*/
 }
 
 
