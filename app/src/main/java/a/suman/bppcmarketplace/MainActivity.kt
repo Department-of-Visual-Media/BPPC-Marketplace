8e25760e7d81b59182d53b00739cd6688f335987
Penguin-12 <f20190824@pilani.bits-pilani.ac.in>
2020-10-07 18:40:26 +0530

    Migrate navigation to android navigation component.

--- app/src/main/java/a/suman/bppcmarketplace/MainActivity.kt
+++ app/src/main/java/a/suman/bppcmarketplace/MainActivity.kt
@@ -8,56 +8,26 @@ import a.suman.bppcmarketplace.UsersList.View.UsersListFragment
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.ismaeldivita.chipnavigation.ChipNavigationBar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val bottom_nav: ChipNavigationBar = findViewById(R.id.chipNavigationBar)
 
-        openFragment(ProductListFragment())
-        bottom_nav.setItemSelected(R.id.products)
-
-        bottom_nav.setOnItemSelectedListener { id :Int->
-            when (id) {
-                R.id.products -> {
-                    val fragment =
-                        ProductListFragment()
-                    openFragment(fragment)
-                }
-                R.id.users -> {
-                    val fragment =
-                        UsersListFragment()
-                    openFragment(fragment)
-                }
-                R.id.upload -> {
-                    val fragment =
-                        UploadFragment()
-                    openFragment(fragment)
-                }
-                R.id.profile -> {
-                    val fragment =
-                        ProfileFragment()
-                    openFragment(fragment)
-                }
-                R.id.chat -> {
-                    val fragment =
-                        ChatListFragment()
-                    openFragment(fragment)
-                }
-            }
-        }
+        navController = Navigation.findNavController(this, R.id.fragment)
+        bottomNavBar.setupWithNavController(navController)
     }
 
-    private fun openFragment(fragment: Fragment) {
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.fragment_container, fragment)
-        transaction.addToBackStack(null)
-        transaction.commit()
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, null)
     }
-
 }
 
 
